6a13cadda20ccf272eea5fb2c0cb9d5184f965a4
mucheru-delvan <mkdelvan9@gmail.com>
2026-08-24 06:38:56 +0300

    Created an expense class

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Expense(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE,
+        related_name='expenses'
+        )
+    title = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
