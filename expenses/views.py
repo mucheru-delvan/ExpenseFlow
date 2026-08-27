d868bc1f2d0508c6f43ca286c3a6c99fff0eab88
mucheru-delvan <mkdelvan9@gmail.com>
2026-08-27 07:05:46 +0300

    Created the time filters for expenses

--- expenses/views.py
+++ expenses/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from datetime import timedelta
+from django.utils import timezone
 
 # Create your views here.
 from rest_framework import generics
@@ -23,4 +25,31 @@ class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Expense.objects.filter(user=self.request.user)
+    queryset = Expense.objects.filter(user=self.request.user)
+
+    period = self.request.query_params.get("period")
+
+    today = timezone.localdate()
+
+    if period == "week":
+        start_date = today - timedelta(days=7)
+        queryset = queryset.filter(date__gte=start_date)
+
+    elif period == "month":
+        start_date = today - timedelta(days=30)
+        queryset = queryset.filter(date__gte=start_date)
+
+    elif period == "3months":
+        start_date = today - timedelta(days=90)
+        queryset = queryset.filter(date__gte=start_date)
+
+    start_date = self.request.query_params.get("start_date")
+    end_date = self.request.query_params.get("end_date")
+
+    if start_date:
+        queryset = queryset.filter(date__gte=start_date)
+
+    if end_date:
+        queryset = queryset.filter(date__lte=end_date)
+
+    return queryset
